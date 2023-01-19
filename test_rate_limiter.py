c019fc32ed626050730b2dd9f88c542a1e0c13f2
Dorothy Brown <43494893+dorothykbrown@users.noreply.github.com>
2023-01-19 18:17:43 +0000

    write a test for the rate limiter functionality

--- test_rate_limiter.py
+++ test_rate_limiter.py
@@ -1,9 +1,19 @@
 import unittest
+from rate_limiter import RateLimiter
 
 class TestRateLimiter(unittest.TestCase):
 
-    def test_rate_limter(self):
-        pass
+    def test_rate_limiter(self):
+        ONE_HOUR_IN_SECONDS = 60 * 60
+        MAX_REQUESTS_PER_HOUR = 100
+        
+        limiter = RateLimiter(MAX_REQUESTS_PER_HOUR, ONE_HOUR_IN_SECONDS)
+
+        request_ip1 = "ip address 1"
+
+        for i in range(MAX_REQUESTS_PER_HOUR):
+            self.assertEqual(limiter.make_request(request_ip1), "Request processed", "Request should be processed")
+        self.assertEqual(limiter.make_request(request_ip1), "Rate limit exceeded. Try again", "Request should not be processed")
 
 if __name__ == '__main__':
     unittest.main()
